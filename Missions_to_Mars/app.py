979869de0d03ce78e7fcd7a2e51ef8813a8d0247
Daewon Kwon <sliwet@gmail.com>
2019-11-03 08:27:02 -0500

    Setting up the environment

---
+++ Missions_to_Mars/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+app = Flask(__name__)
+
+app.config["MONGO_URI"] = "mongodb://localhost:27017/dbname"
+mongo = PyMongo(app)
+
+@app.route("/")
+def index():
+    listings = mongo.db.collname.find_one()
+    return render_template("index.html", listings=listings)
+
+@app.route("/scrape")
+def scraper():
+    listings = mongo.db.collname
+    listings_data = scrape_mars.scrape()
+    listings.update({}, listings_data, upsert=True)
+    return redirect("/", code=302)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ Missions_to_Mars/scrape_mars.py
@@ -0,0 +1,22 @@
+from splinter import Browser
+from bs4 import BeautifulSoup
+
+def init_browser():
+    executable_path = {"executable_path": "chromedriver"}
+    return Browser("chrome", **executable_path, headless=False)
+
+def scrape():
+    browser = init_browser()
+
+    url = "https://raleigh.craigslist.org/search/hhh?max_price=1500&availabilityMode=0"
+    browser.visit(url)
+
+    html = browser.html
+    soup = BeautifulSoup(html, "html.parser")
+
+    listings = {}
+    listings["headline"] = soup.find("a", class_="result-title").get_text()
+    listings["price"] = soup.find("span", class_="result-price").get_text()
+    listings["hood"] = soup.find("span", class_="result-hood").get_text()
+
+    return listings
