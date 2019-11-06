f1a9991d41d9366bc9240f9bda1b74515bc28278
Daewon Kwon <sliwet@gmail.com>
2019-11-05 22:51:23 -0500

    Major update

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -3,11 +3,20 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import time
 
+# This is for debugging
+
+def savetofile(contents):
+    file = open('_temporary.txt',"w",encoding="utf-8")
+    file.write(contents)
+    file.close()
+
+
 def scrape():
     executable_path = {"executable_path": "chromedriver"}
     browser = Browser("chrome", **executable_path, headless=False)
 
     # NASA Mars News
+
     url = 'https://mars.nasa.gov/news/'
 
     browser.visit(url)
@@ -33,6 +42,7 @@ def scrape():
     url = base_url + '/spaceimages/?search=&category=Mars'
 
     browser.visit(url)
+    time.sleep(3)
     html = browser.html
     soup = BeautifulSoup(html, 'html.parser')
 
@@ -43,6 +53,7 @@ def scrape():
     url = 'https://twitter.com/marswxreport?lang=en'
 
     browser.visit(url)
+    time.sleep(3)
     html = browser.html
     soup = BeautifulSoup(html, 'html.parser')
 
@@ -53,24 +64,65 @@ def scrape():
         i = i+1
     mars_weather = streamitems[i].find('div',class_='js-tweet-text-container')
     mars_weather = mars_weather.find('p').text
+    mars_weather = mars_weather.replace('\n', '')
 
     # Mars facts
 
     url = 'https://space-facts.com/mars/'
+    browser.visit(url) # not necessary, but added for checking the operation
+    time.sleep(3)
 
-    df = pd.read_html(url)[1]
-    df = df.rename(columns={0:"Description", 1:"Value"})
-    df = df.set_index("Description")
-    marsfacts_html = df.to_html().replace('\n', '')
+    dfs = pd.read_html(url)
+    for df in dfs:
+        try:
+            df = df.rename(columns={0:"Description", 1:"Value"})
+            df = df.set_index("Description")
+            marsfacts_html = df.to_html().replace('\n', '')
+            df.to_html('marsfacts.html') # to save to a file to test
+            break
+        except:
+            continue
+
+    # Mars Hemispheres
+
+    base_url = 'https://astrogeology.usgs.gov'
+    url = base_url + '/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+
+    browser.visit(url)
+    time.sleep(3)
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
+
+    items = soup.find_all('div', class_='item')
+
+    urls = []
+    titles = []
+    for item in items:
+        urls.append(base_url + item.find('a')['href'])
+        titles.append(item.find('h3').text.strip())
+
+    img_urls = []
+    for oneurl in urls:
+        browser.visit(oneurl)
+        html = browser.html
+        soup = BeautifulSoup(html, 'html.parser')
+        oneurl = base_url+soup.find('img',class_='wide-image')['src']
+        img_urls.append(oneurl)
+
+    hemisphere_image_urls = []
+
+    for i in range(len(titles)):
+        hemisphere_image_urls.append({'title':titles[i],'img_url':img_urls[i]})
 
     # Assigning scraped data to a page
-
+    
     marspage = {}
     marspage["news_title"] = news_title
     marspage["news_p"] = news_p
-    marspage["featured_img"] = featured_image_url
+    marspage["featured_image_url"] = featured_image_url
     marspage["mars_weather"] = mars_weather
-    marspage["mars_facts"] = marsfacts_html
+    marspage["marsfacts_html"] = marsfacts_html
+    marspage["hemisphere_image_urls"] = hemisphere_image_urls
 
     return marspage
     
